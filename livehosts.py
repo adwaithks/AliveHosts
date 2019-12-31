0686f4790c7d585487d4dac239e0cd765924ddd8
Adwaith <45693088+adwaithks@users.noreply.github.com>
2020-01-01 01:35:03 +0530

    Update livehosts.py

--- livehosts.py
+++ livehosts.py
@@ -1,15 +1,27 @@
 import requests
 import sys
-import time
+from threading import Thread
+
+
+class colors: 
+    reset='\033[0m'
+    class fg: 
+        red='\033[31m'
+        orange='\033[33m'
+        blue='\033[34m'
+        yellow='\033[93m'
+        pink='\033[95m'
+
 
 file = sys.argv[1]
 templist = []
+threads = []
+livehosts = []
 
 
-print "===========Alive Hosts==========	-Adwaith"
+print colors.fg.yellow + "===========Alive Hosts==========	-Adwaith" + colors.reset
 
-def connect(line):	
-
+def connect(line,livehosts):	
 	sub1 = "https://" + line
 	sub2 = "http://" + line
 	sub1 = sub1.rstrip()
@@ -21,7 +33,8 @@ def connect(line):
 	except Exception:
 		return
 	if httpsstatus == 200:
-		print sub1 
+		print colors.fg.orange + sub1 + colors.reset 
+		livehosts.append(sub1)
 
 	try:
 		http = requests.get(sub2)
@@ -29,7 +42,10 @@ def connect(line):
 	except Exception:
 		return
 	if httpstatus == 200:
-		print sub2
+		print colors.fg.blue + sub2 + colors.reset
+		livehosts.append(sub2)
+
+print colors.fg.pink + "[+] Fetching subdomains from {0}".format(file) + colors.reset
 
 
 filecontent = open(file, "r")
@@ -39,5 +55,16 @@ for line in filecontent:
 	
 for i in range(len(templist)):
 	line = templist[i].replace("'", "")
-	connect(line)
+	process = Thread(target=connect, args=[line,livehosts])
+	process.start()
+	threads.append(process)
 
+for process in threads:
+	process.join()
+
+print colors.fg.yellow + "[+] Saving results to livehosts.txt" + colors.reset
+file = open("livehosts.txt",'w')
+for i in range(len(livehosts)):
+	file.write(livehosts[i])
+	file.write("\n")
+file.close()
